4cc830b9beb10710e970151096b7967c3467ab37
msilin <msilin@235x25.ad.calpoly.edu>
2019-03-09 18:18:41 -0800

    first commit

---
+++ SoccorShop.java
@@ -0,0 +1,25 @@
+import java.sql.*; 
+import java.util.*; 
+
+class SoccorShop
+{ 
+	static Connection connect;
+   public static void main(String args[])
+   {
+	   try {
+		   Class.forName("om.mysql.jdbc.Driver");
+		   	   //ambari-head.csc.calpoly.edu
+
+		   connect = DriverManager.getConnection("jdbc:mysl//localhost:3306/testdb?user=root&password=...");
+		   
+		   Statement statement = connect.createStatement();
+		   ResultSet rs = statement.executeQuery( "SELECT * FROM BOATS");
+		   while (rs.next()) { 
+		   	String boatName = rs.getString(2);
+		   	System.out.println("Boat name= " + boatName);
+		   }
+	   } catch (Exception e) {System.out.println(e.getMessage());}
+   }
+}
+
+class Play
