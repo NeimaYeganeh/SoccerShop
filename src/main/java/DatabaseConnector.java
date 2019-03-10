7fdc6d60ac665602096be6c557f53edf1d1518ac
Justis Mackaoui <justismackaoui@gmail.com>
2019-03-10 15:47:02 -0700

    added getUser from db

--- src/main/java/DatabaseConnector.java
+++ src/main/java/DatabaseConnector.java
@@ -1,5 +1,8 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class DatabaseConnector {
 
@@ -21,5 +24,50 @@ public class DatabaseConnector {
 		}
 		return null;
 		
+	}
+	
+	// fetch from DB
+	public static User getUser(String email) {
+		
+		User user = null;
+		Connection con = null;;
+		PreparedStatement stmt = null;;
+		ResultSet rs = null;
+		
+		try {
+			
+			con = getConnection();
+			String stmtString = "SELECT * FROM Users WHERE Users.email=?";
+			stmt = con.prepareStatement(stmtString);
+			stmt.setString(1, email);
+			rs = stmt.executeQuery();
+		
+			while (rs.next()) {
+				
+				String userId = rs.getString("userID");
+				String firstName = rs.getString("firstName");
+				String lastName = rs.getString("lastName");
+				User.UserType type = User.UserType.valueOf(rs.getString("type"));
+				
+				user = new User(userId, email, lastName, firstName, type);
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				stmt.close();
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}	
+		}
+		
+		return user;
+	}
+	
+	// store in DB
+	public static void storeUser(User user) {
+	
 	}
 }
