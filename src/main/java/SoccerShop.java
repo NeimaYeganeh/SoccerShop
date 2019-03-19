675d9ea8f0c8404106704fec2f0286ae9233e96a
Michael Silin <msilin@calpoly.edu>
2019-03-19 12:24:56 -0700

    login

--- src/main/java/SoccerShop.java
+++ src/main/java/SoccerShop.java
@@ -1,14 +1,18 @@
+import static DatabaseConnector.getConnection;
 import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.sql.*;
 
 public class SoccerShop {
 
-    public enum ShopState {Start, UsageError, Exit};
+    public enum ShopState {
+        START, USAGEMESSAGE, EXIT, STORE, CART, DONE, LOGIN, VIEWITEMS
+    }
 
     private ShopState state;
 
 	public SoccerShop() {
-	    this.state = ShopState.Start;
+	    this.state = ShopState.START;
     }
 
     // Main UI for the SoccerShop
@@ -22,10 +26,11 @@ public class SoccerShop {
         *   main control flow of program
         *   quits program when "Exit" command entered
         */
+        
         while (true) {
 
-            response = doAction(state); // executes the action, returns next prompt
-            System.out.print(response); // displays prompt
+            response = doAction(state); // executes the action
+            System.out.println(response); // displays next prompt
             input = sc.nextLine();      // reads user input from command line
             state = parseInput(input);  // converts the user input into a Case
         }
@@ -37,29 +42,95 @@ public class SoccerShop {
 	    String response = null;
 
         switch (state) {
-            case Start:
-                response = "yo what up: ";
-
-            case UsageError:
+            case START:
+                welcomeMessage();
                 response = usageMessage();
-
-            case Exit:
+                break;
+            case USAGEMESSAGE:
+                response = usageMessage();
+                break;
+            case EXIT:
+                System.exit(0);
+                break;
+            case CART:
+                break;
+            case STORE:
+                break;
+            case LOGIN:
+                login();
+                break;
+            case VIEWITEMS:
+                break;
+            case DONE:
                 System.exit(0);
         }
         return response;
     }
 
+    
+    private ShopState login()
+    {
+        Scanner sc = new Scanner(new InputStreamReader(System.in));
+
+        Connection con = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        
+        User.UserType type = null;
+        
+        String email = null;
+        String password = null;
+        
+        System.out.println("Please enter your username");
+        email = sc.nextLine();
+        
+        System.out.println("Please enter your password");
+        password = sc.nextLine();
+        
+        try {
+			
+                con = DatabaseConnector.getConnection();
+                String stmtString = "SELECT * FROM Users WHERE Users.email=? and Users.password=?";
+                stmt = con.prepareStatement(stmtString);
+                stmt.setString(1, email);
+                stmt.setString(2, password);
+                rs = stmt.executeQuery();
+
+                type = User.UserType.valueOf(rs.getString("type"));
+                
+        } catch (SQLException e) {
+                e.printStackTrace();
+        } finally {
+                try {
+                        stmt.close();
+                        con.close();
+                } catch (SQLException e) {
+                        e.printStackTrace();
+                }	
+        }
+        if (type.equals("Admin"))
+            return ShopState.STORE;
+        else if (type.equals("Employee"))
+            return ShopState.STORE;
+        else 
+            return ShopState.STORE;
+    }
+    
     private ShopState parseInput(String input) {
+
 	    ShopState state = null;
+	    input = input.replaceAll("\\s+","");    // get rid of whitespace
+        input = input.toUpperCase();    // make case-insensitive
 
 	    try {
-	        return ShopState.valueOf(input);
+	        state = ShopState.valueOf(input);
         }
 	    catch (IllegalArgumentException iae){
-	        state = ShopState.UsageError;
+	        state = ShopState.USAGEMESSAGE;     // invalid input (no matching ShopState)
         }
 	    catch (Exception e) {
-	        e.printStackTrace();
+	        e.printStackTrace();    // program actually crashed -- shouldn't happen
+            System.exit(1);  // exit with error code flagged
         }
 	    return state;
     }
@@ -67,6 +138,10 @@ public class SoccerShop {
     private String usageMessage() {
 	    String msg = "";    // fill in with options
         return msg;
+    }
+
+    private void welcomeMessage() {
+	    System.out.println("Welcome to the SoccerShop!");
     }
 	
 }
